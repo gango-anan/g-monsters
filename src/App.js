c621de5319f6f1565c852ddbe6076ede3e807954
Galiwango Ananiya <galiwango101@gmail.com>
2022-01-11 02:55:02 +0300

    Refactor search input into its own component and add styles to it.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 import CardList from "./components/card-list/card-list";
+import searchBoxComponent from "./components/search-box/search-box.component";
+import SearchBox from "./components/search-box/search-box.component";
 
 class App extends Component {
   constructor() {
@@ -24,7 +26,7 @@ class App extends Component {
     );
     return(
         <div className="App">
-          <input type="search" placeholder='Search monsters' onChange={e => this.setState({searchField: e.target.value})} />
+          <SearchBox placeholder='Search monsters' handleChange={(e) => this.setState({searchField: e.target.value})} />
           <CardList monsters={filteredMonsters}/>
         </div>
     )
